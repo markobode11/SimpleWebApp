7b6d04e641834c2b8addfa2ce3e01fd886261824
marko.bode <marko.bode@netgroup.com>
2022-04-20 18:25:10 +0300

    .NET 6 upgrade.

---
+++ TestApp/TestApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿namespace TestApp.Controllers
+{
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        public Microsoft.AspNetCore.Mvc.ActionResult Index()
+        {
+            return View();
+        }
+
+        public Microsoft.AspNetCore.Mvc.ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public Microsoft.AspNetCore.Mvc.ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
